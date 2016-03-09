95a8b346673921d3355716dffd5a4ec4d672eb48
takecare <rui@vazteixeira.org>
2016-03-09 19:04:16 +0000

    draw square based on key events

--- uglyvaders.love/main.lua
+++ uglyvaders.love/main.lua
@@ -1,11 +1,23 @@
-function love.load()
+x,y = 0,0
 
+function love.load()
+    -- ...
 end
 
 function love.update(dt)
-
+    if love.keyboard.isDown("right") then
+        x = x + 1
+    elseif love.keyboard.isDown("left") then
+        x = x - 1
+    end
+    if love.keyboard.isDown("up") then
+        y = y - 1
+    elseif love.keyboard.isDown("down") then
+        y = y + 1
+    end
 end
 
 function love.draw()
-
+    love.graphics.setColor(0, 0, 255)
+    love.graphics.rectangle("fill", x, y, 100, 100)
 end
