c34e365741c2b5214780efc3dca22c2240dffb13
takecare <rui@vazteixeira.org>
2016-03-09 19:07:29 +0000

    pull out pos into player table

--- uglyvaders.love/main.lua
+++ uglyvaders.love/main.lua
@@ -1,4 +1,4 @@
-x,y = 0,0
+player = {x=0,y=0,bullets={}}
 
 function love.load()
     -- ...
@@ -6,18 +6,18 @@ end
 
 function love.update(dt)
     if love.keyboard.isDown("right") then
-        x = x + 1
+        player.x = player.x + 1
     elseif love.keyboard.isDown("left") then
-        x = x - 1
+        player.x = player.x - 1
     end
     if love.keyboard.isDown("up") then
-        y = y - 1
+        player.y = player.y - 1
     elseif love.keyboard.isDown("down") then
-        y = y + 1
+        player.y = player.y + 1
     end
 end
 
 function love.draw()
     love.graphics.setColor(0, 0, 255)
-    love.graphics.rectangle("fill", x, y, 100, 100)
+    love.graphics.rectangle("fill", player.x, player.y, 100, 100)
 end
