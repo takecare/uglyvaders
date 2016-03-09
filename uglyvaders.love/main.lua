efbcf91a6fadf289039f7e4d0d138957c8837df1
takecare <rui@vazteixeira.org>
2016-03-09 19:46:14 +0000

    set a limit on the fire rate and delete bullets that are offscreen

--- uglyvaders.love/main.lua
+++ uglyvaders.love/main.lua
@@ -1,12 +1,20 @@
 DEBUG = true
 
-player = {x=0,y=400,w=100,h=20,bullets={}}
+player = {x=0,y=400,w=100,h=20,bw=10,bh=10,bt=os.time(),bullets={}}
 
 function love.load()
     player.fire = function()
-        d("fire")
-        bullet = {x=player.x+player.w/2,y=player.y-player.h/2}
-        table.insert(player.bullets, bullet)
+        currentTime = os.time()
+        diffTime = currentTime - player.bt
+        d("curr=" .. currentTime .. " diff=" .. diffTime)
+        if diffTime >= 1 then
+            d("fire!")
+            player.bt = currentTime
+            bullet = {x=player.x+player.w/2,y=player.y-player.h/2}
+            table.insert(player.bullets, bullet)
+        else
+            d("no fire")
+        end
     end
 end
 
@@ -16,13 +24,17 @@ function love.update(dt)
     elseif love.keyboard.isDown("left") then
         player.x = player.x - 1
     end
-    --[[if love.keyboard.isDown("up") then
-        player.y = player.y - 1
-    elseif love.keyboard.isDown("down") then
-        player.y = player.y + 1
-    end]]--
+
     if love.keyboard.isDown("space") then
         player.fire()
+    end
+
+    for i,b in pairs(player.bullets) do
+        if b.y < -player.bh then
+            table.remove(player.bullets, i)
+        else
+            b.y = b.y - 2
+        end
     end
 end
 
@@ -36,7 +48,7 @@ function love.draw()
     for _,b in pairs(player.bullets) do
         love.graphics.setColor(255, 255, 255)
         d("drawing bullet. total bullets = " .. #player.bullets)
-        love.graphics.rectangle("fill", b.x, b.y, 10, 10)
+        love.graphics.rectangle("fill", b.x, b.y, player.bw, player.bh)
     end
 
 end
