580524e8ae8199663438ba89249b58bcba2eb50f
takecare <rui@vazteixeira.org>
2016-03-09 19:25:08 +0000

    draw bullets

--- uglyvaders.love/main.lua
+++ uglyvaders.love/main.lua
@@ -1,11 +1,11 @@
 DEBUG = true
 
-player = {x=0,y=400,bullets={}}
+player = {x=0,y=400,w=100,h=20,bullets={}}
 
 function love.load()
     player.fire = function()
         d("fire")
-        bullet = {x=player.x,player.y}
+        bullet = {x=player.x+player.w/2,y=player.y-player.h/2}
         table.insert(player.bullets, bullet)
     end
 end
@@ -27,8 +27,18 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(0, 0, 255)
-    love.graphics.rectangle("fill", player.x, player.y, 100, 100)
+    
+    -- draw player
+    love.graphics.setColor(200, 200, 200)
+    love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
+
+    -- draw bullets
+    for _,b in pairs(player.bullets) do
+        love.graphics.setColor(255, 255, 255)
+        d("drawing bullet. total bullets = " .. #player.bullets)
+        love.graphics.rectangle("fill", b.x, b.y, 10, 10)
+    end
+
 end
 
 function d(s)
